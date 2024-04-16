f868a9a87f816aac9b49bb58b60b34ac2d0acf76
Yinka ☯️ <realjohnadex@outlook.com>
2024-04-16 13:33:33 +0000

    Add docstrings to test_amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-""" """
+"""
+This module contains unit tests for the Amenity class.
+"""
+
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """
+    This class contains unit tests for the Amenity class.
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Initializes a test instance of the Amenity class.
+        """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """
+        Test case to check the type of the 'name' attribute of a new Amenity instance.
+        """
         new = self.value()
         self.assertEqual(type(new.name), str)
