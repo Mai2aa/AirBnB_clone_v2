c9394065c6b99cf546bb4c331d9c4f01ef1deefc
Mai2aa <mysaadil0@gmail.com>
2024-05-23 15:48:14 -0700

    Python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,9 +21,10 @@ def c_route(text):
     return "C " + text.replace('_', ' ')
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_route(text="is cool"):
-    return "Python" + text.replace('_', ' ')
+    return "Python " + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
